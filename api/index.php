fd9df3beae7c0c689013af5b96c32d3cfba6a754
Atanu Pal <atanupal@live.com>
2022-10-11 19:11:50 +0530

    post class created

--- api/index.php
+++ api/index.php
@@ -3,25 +3,26 @@ require "../start.php";
 
 use Src\Post;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: appliaction/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: appliaction/json; charset=UTF-8");
+// header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+// header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
+// var_dump($uri);
+
 // endpoints starting with `/post` or `/posts` for GET shows all posts
 // everything else results in a 404 Not Found
 
-if ($uri[1] !== 'post') {
-    if ($uri[1] !== 'posts') {
+if ($uri[4] !== 'post') {
+    if ($uri[4] !== 'posts') {
         header("HTTP/1.1 404 Not Found");
         exit();
     }
 }
-
 // endpoints starting with `/posts` for POST/PUT/DELETE results in a 404 Not Found
 if ($uri[1] == 'posts' && isset($uri[2])) {
     header("HTTP/1.1 404 Not Found");
--- src/Post.php
+++ src/Post.php
@@ -93,4 +93,135 @@ class Post
         $response['body'] = json_encode($result);
         return $response;
     }
+
+    private function create_post()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validate_post($input)) {
+            return $this->unprocessable_entity_response();
+        }
+        $title = $input['title'];
+        $body = $input['body'];
+        $author = $input['author'];
+        $author_picture = 'https://secure.gravatar.com/avatar/' . md5(strtolower($input['author'])) . '.png?s=200';
+
+        $sql = "INSERT INTO post 
+                VALUES(
+                    '$title', 
+                    '$body', 
+                    '$author', 
+                    '$author_picture'
+                )";
+
+        try {
+            $result = $this->conn->query($sql);
+
+            if (!$result) {
+                throw new Exception("Error : " . $result->error);
+            }
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = json_encode(['message' => 'Post Created']);
+        return $response;
+    }
+
+    private function update_post($id)
+    {
+        $result = $this->find($id);
+        if (!$result) {
+            return $this->not_found_response();
+        }
+
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validate_post($input)) {
+            return $this->unprocessable_entity_response();
+        }
+
+        $title = $input['title'];
+        $body = $input['body'];
+        $author = $input['author'];
+        $author_picture = 'https://secure.gravatar.com/avatar/' . md5(strtolower($input['author'])) . '.png?s=200';
+
+        $sql = "UPDATE post 
+                SET 
+                    title='$title', 
+                    body='$body', 
+                    uthor='$author', 
+                    author_picture='$author_picture' 
+                WHERE id = '$id'";
+
+        try {
+            $result = $this->conn->query($sql);
+
+            if (!$result) {
+                throw new Exception("Error : " . $result->error);
+            }
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode(array('message' => 'Post Updated!'));
+        return $response;
+    }
+
+    private function delete_post($id)
+    {
+        $result = $this->find($id);
+        if (!$result) {
+            return $this->not_found_response();
+        }
+
+        $sql = "DELETE FROM post WHERE id='$id'";
+
+        try {
+            $result = $this->conn->query($sql);
+
+            if (!$result) {
+                throw new Exception("Error : " . $result->error);
+            }
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode(array('message' => 'Post Deleted!'));
+        return $response;
+    }
+
+    public function process_request()
+    {
+        switch ($this->request_method) {
+            case 'GET':
+                if ($this->post_id) {
+                    $response = $this->get_post($this->post_id);
+                } else {
+                    $response = $this->get_all_posts();
+                }
+                break;
+
+            case 'POST':
+                $response = $this->create_post();
+                break;
+
+            case 'PUT':
+                $response = $this->update_post($this->post_id);
+                break;
+
+            case 'DELETE':
+                $response = $this->delete_post($this->post_id);
+                break;
+            default:
+                $response = $this->not_found_response();
+                break;
+        }
+
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
 }
